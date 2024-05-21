b512915917c0c6f9e60b55dad6286274185fa000
ashish6298 <ashishgoswami6298@gmail.com>
2024-05-21 21:08:24 +0530

    new

--- TIC TAC TOE/tic-tac-toe.py
+++ TIC TAC TOE/tic-tac-toe.py
@@ -1,4 +1,5 @@
 board=[" " for x in range(9)]
+
 def print_board():
     row1="|{}|{}|{}|".format(board[0],board[1],board[2])
     row2="|{}|{}|{}|".format(board[3],board[4],board[5])
@@ -8,31 +9,37 @@ def print_board():
     print(row2)
     print(row3)
     print()
+    
+    
 def player_move(icon):
     if icon=="X":
         number=1
-    elif icon=="O":
+    elif icon=="0":
         number=2
-    print("Your turn payer{}".format(number))
-    choice=int(input("enter the move(1-9):").strip())
+    print("Your tunr player {}".format(number))
+    choice=int(input("Ente the value(1-9) : ").strip())
     if board[choice-1]==" ":
-        board[choice-1]=icon
+        board[choice-1]==icon
     else:
         print()
-        print("that space is  taken")
-def is_victory(icon):
-    if (board[0]==icon and board[1]==icon and board[2]==icon) or \
-    (board[3]==icon and board[4]==icon and board[5]==icon)or \
-    (board[6]==icon and board[7]==icon and board[8]==icon)or \
-    (board[0]==icon and board[3]==icon and board[6]==icon)or \
-    (board[1]==icon and board[4]==icon and board[7]==icon)or \
-    (board[2]==icon and board[5]==icon and board[8]==icon)or \
-    (board[0]==icon and board[4]==icon and board[8]==icon)or  \
+        print("Space is already taken")
+        
+        
+def victory(icon):
+    if(board[0]==icon and board[1]==icon and board[2]==icon) or \
+    (board[3]==icon and board[4]==icon and board[5]==icon) or \
+    (board[6]==icon and board[7]==icon and board[8]==icon) or \
+    (board[0]==icon and board[3]==icon and board[6]==icon) or \
+    (board[1]==icon and board[4]==icon and board[7]==icon) or \
+    (board[2]==icon and board[5]==icon and board[8]==icon) or \
+    (board[0]==icon and board[4]==icon and board[8]==icon) or \
     (board[2]==icon and board[4]==icon and board[6]==icon):
-         return True
+        return True
     else:
         return False
-def is_drawn():
+    
+    
+def draw():
     if " " not in board:
         return True
     else:
@@ -41,67 +48,18 @@ while True:
     print_board()
     player_move("X")
     print_board()
-    if is_victory("X"):
-        print("X wins! congrats")
+    if victory("X"):
+        print("X wins")
         break
-    elif is_drawn():
-        print("its  a drawn")
+    elif draw():
+        print("Draw")
         break
-    player_move("O")
-    if  is_victory("O"):
+    player_move("0")
+    if victory("0"):
         print_board()
-        print("O wins!congrats")
+        print("0 Wins")
         break
-    elif is_drawn():
-        print("its  a drawn")
+    elif draw():
+        print("drwa")
         break
-<<<<<<< HEAD
-
-=======
->>>>>>> 4e669e7206f2225edc01868b63e93526f71036c4
-
-# ------------------------------------------------------------
-# OUTPUT
-
-# | | | |
-# | | | |
-# | | | |
-
-# Your turn payer1
-# enter the move(1-9):1
-
-# |X| | |
-# | | | |
-# | | | |
-
-# Your turn payer2
-# enter the move(1-9):2
-
-# |X|O| |
-# | | | |
-# | | | |
-
-# Your turn payer1
-# enter the move(1-9):5
-
-# |X|O| |
-# | |X| |
-# | | | |
-
-# Your turn payer2
-# enter the move(1-9):3
-
-# |X|O|O|
-# | |X| |
-# | | | |
-
-# Your turn payer1
-# enter the move(1-9):9
-
-# |X|O|O|
-# | |X| |
-# | | |X|
-
-# X wins! congrats
-
-# -------------------------------------------------------------------
+        
